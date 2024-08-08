c21b5eae501573cff0989aabded95fad326a5bdc
Marcelo Carvalho <silva.marcelocarvalho@gmail.com>
2024-08-08 17:52:42 -0300

    add offline page

--- sw.js
+++ sw.js
@@ -2,25 +2,34 @@ const CACHE_NAME = 'olympics-cache-v1';
 const urlsToCache = [
     '/',
     '/index.html',
-    '/styles.css',
-    '/script.js',
+    '/offline.html',
+    '/css/styles.css',
+    '/scripts/script.js',
+    '/scripts/fetchCountries.js',
     '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
+        caches.open('olympics-static-v1').then(cache => {
+            return cache.addAll(urlsToCache);
+        })
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
+        caches.match(event.request).then(cachedResponse => {
+            // Se encontrar no cache, retorne-o
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+
+            // Tente buscar na rede
+            return fetch(event.request).catch(() => {
+                // Se a rede falhar, retorne uma resposta padrão, como uma página offline
+                return caches.match('/offline.html');
+            });
+        })
     );
 });
